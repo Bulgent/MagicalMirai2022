019ea62288b10a1e85dec60507593bfc4eb16a40
Bulgent <moritakakuro0305@gmail.com>
2022-07-18 21:28:24 +0900

    Project Arcturus

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1437042.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+SCRIPTS = [ 92692669, 86106893, 86106894, 86106898, 86106899, 86106901, 86106892, 86106891, 86106896, 86106895, 86169999, 86170917, 86175143, 90326091, 92089269, 92238304, 92635642, 92639201, 92729551, 93610808, 93626133, 93706138, 93829086, 93831258, 93835211, 93831259, 93836250, 93833369, 93840792, 93835213, 93831531, 93832015 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/86107126/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/86107127/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/86107125/1/ammo.js', 'preload' : true},
+];
